2bcea951e5a9059d47f9c9ef38871afdc6fa403b
Tomy Falgui <tomyfalgui@gmail.com>
2023-04-01 12:50:24 +0800

    add neighbor indices

--- board/board.go
+++ board/board.go
@@ -36,3 +36,59 @@ func Render(grid [][]int) string {
 
 	return sb.String()
 }
+
+func NextState(grid [][]int) [][]int {
+	// yDimMax := len(grid)
+	// xDimMax := len(grid[0])
+	// nextState := [][]int{}
+
+	return grid
+}
+
+/*
+*
+(x-1, y-1)
+(x, y-1)
+(x+1, y-1)
+(x-1, y)
+(x+1, y)
+(x-1, y+1)
+(x, y+1)
+(x+1, y+1)
+*/
+func NeighborIndices(x, y, maxX, maxY int) [][]int {
+	neighbors := make([][]int, 8)
+
+	// y + 1
+	// x + 1
+	// y - 1
+	// x - 1
+	yInc := y + 1
+	xInc := x + 1
+	yDec := y - 1
+	xDec := x - 1
+
+	if yInc >= maxY {
+		yInc = 0
+	}
+	if xInc >= maxX {
+		xInc = 0
+	}
+	if yDec < 0 {
+		yDec = maxY - 1
+	}
+	if xDec < 0 {
+		xDec = maxX - 1
+	}
+
+	neighbors[0] = []int{yDec, xDec}
+	neighbors[1] = []int{yDec, x}
+	neighbors[2] = []int{yDec, xInc}
+	neighbors[3] = []int{y, xDec}
+	neighbors[4] = []int{y, xInc}
+	neighbors[5] = []int{yInc, xDec}
+	neighbors[6] = []int{yInc, x}
+	neighbors[7] = []int{yInc, xInc}
+
+	return neighbors
+}
--- board/board_test.go
+++ board/board_test.go
@@ -3,6 +3,8 @@ package board_test
 import (
 	"testing"
 
+	cmp "github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	board "github.com/tomyfalgui/game-of-life-go/board"
 )
 
@@ -44,10 +46,76 @@ func TestRenderGrid(t *testing.T) {
 	}
 }
 
+func TestGetNeighborIndices_XYPair(t *testing.T) {
+	t.Parallel()
+	maxY := 10
+	maxX := 5
+
+	currYIndex1 := 0
+	currXIndex1 := 0
+
+	// in (Y, X) form
+	wantNeighborIndices1 := [][]int{
+		{0, 1},
+		{1, 0},
+		{1, 1},
+		{9, 4},
+		{9, 0},
+		{9, 1},
+		{0, 4},
+		{1, 4},
+	}
+	gotNeigborIndices1 := board.NeighborIndices(currXIndex1, currYIndex1, maxX, maxY)
+	if !cmp.Equal(
+		wantNeighborIndices1,
+		gotNeigborIndices1,
+		cmpopts.SortSlices(func(x, y [][]int) bool {
+			return x[0][0] == y[0][0]
+		}),
+	) {
+		t.Errorf("diff NeighborIndices1: %v", cmp.Diff(wantNeighborIndices1, gotNeigborIndices1))
+	}
+}
+
 // behaviors
 /**
-Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
+1. Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
 Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
 Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
 Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
 */
+
+func TestNextStateCells_WithZeroOrOneLiveNeighbors_ShouldBeDead(t *testing.T) {
+	t.Parallel()
+
+	// base case
+	initState1 := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	wantState1 := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	gotState1 := board.NextState(initState1)
+	if !cmp.Equal(wantState1, gotState1) {
+		t.Errorf("state1 diff: %v", cmp.Diff(wantState1, gotState1))
+	}
+
+	initState2 := [][]int{
+		{0, 1, 0},
+		{0, 0, 0},
+		{1, 1, 0},
+	}
+	wantState2 := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	gotState2 := board.NextState(initState2)
+	if !cmp.Equal(wantState2, gotState2) {
+		t.Errorf("state2 diff: %v", cmp.Diff(wantState2, gotState2))
+	}
+}
